7fe57fff578014b1445fe19f51120ab225979933
Russell Bicknell <bicknellr@google.com>
2018-07-19 10:50:45 -0700

    Fix `iron-pages.js` comments.

--- iron-pages.js
+++ iron-pages.js
@@ -7,6 +7,13 @@ The complete set of contributors may be found at http://polymer.github.io/CONTRI
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
+import '@polymer/polymer/polymer-legacy.js';
+
+import { IronResizableBehavior } from '@polymer/iron-resizable-behavior/iron-resizable-behavior.js';
+import { IronSelectableBehavior } from '@polymer/iron-selector/iron-selectable.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+
 /**
 `iron-pages` is used to select one of its children to show. One use is to cycle through a list of
 children "pages".
@@ -27,20 +34,8 @@ Example:
     </script>
 
 @group Iron Elements
-@hero hero.svg
 @demo demo/index.html
 */
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
-import '@polymer/polymer/polymer-legacy.js';
-
-import { IronResizableBehavior } from '@polymer/iron-resizable-behavior/iron-resizable-behavior.js';
-import { IronSelectableBehavior } from '@polymer/iron-selector/iron-selectable.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 Polymer({
   _template: html`
     <style>
